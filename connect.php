14edaadaaf24c55df982bb56bcdd9146e79e2685
bornasinovcic <sinovcic.borna@gmail.com>
2022-06-26 17:37:47 +0200

    modified: administracija.php, clanak.php, connect.php, index.php, kategorija.php, registracija.php, skripta.php, unos.php and made a new file navigation.php

--- connect.php
+++ connect.php
@@ -3,7 +3,6 @@
     $dbuser = "root";
     $dbpass = "";
     $db = "projekt";
-    $dbc = mysqli_connect($dbhost, $dbuser, $dbpass, $db) or
-        die('Error connecting to MySQL server' . mysqli_error());
+    $dbc = mysqli_connect($dbhost, $dbuser, $dbpass, $db) or die('Error connecting to MySQL server' . mysqli_error());
     mysqli_set_charset($dbc, "utf8");
 ?>
--- index.php
+++ index.php
@@ -9,45 +9,7 @@
 </head>
 <body>
     <?php require_once 'header.php'?>
-    <div class="container bg-white text-center">
-        <div class="row">
-            <div class="col-xxl-3 col-xl-3 col-lg-4 col-md-6 col-sm-12 fs-5 fw-bold">
-                <a href="./index.php">
-                    <div class="m-2">
-                        Home
-                    </div>
-                </a>
-            </div>
-            <div class="col-xxl-2 col-xl-3 col-lg-4 col-md-6 col-sm-12 fs-5 fw-bold">
-                <a href="./kategorija.php?category=U.S.">
-                    <div class="m-2">
-                        U.S.
-                    </div>
-                </a>
-            </div>
-            <div class="col-xxl-2 col-xl-3 col-lg-4 col-md-12 col-sm-12 fs-5 fw-bold">
-                <a href="./kategorija.php?category=World">
-                    <div class="m-2">
-                        World
-                    </div>
-                </a>
-            </div>
-            <div class="col-xxl-2 col-xl-3 col-lg-12 col-md-12 col-sm-12 fs-5 fw-bold">
-                <a href="./administracija.php">
-                    <div class="m-2">
-                        Administracija
-                    </div>
-                </a>
-            </div>
-            <div class="col-xxl-3 col-xl-12 col-lg-12 col-md-12 col-sm-12 fs-5 fw-bold">
-                <a href="./unos.php">
-                    <div class="m-2">
-                        Unos
-                    </div>
-                </a>
-            </div>
-        </div>
-    </div>
+    <?php require_once 'navigation.php'?>
     <article>
         <div class="container bg-white mt-1">
             <div class="row">
