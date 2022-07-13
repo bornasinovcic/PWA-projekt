f7825be086ec00b53529fd41244d665e7809c331
bornasinovcic <sinovcic.borna@gmail.com>
2022-07-13 22:24:23 +0200

    modified: php/connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,4 @@
 <?php
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "";
-    $db = "projekt";
-    $dbc = mysqli_connect($dbhost, $dbuser, $dbpass, $db) or die('Error connecting to MySQL server' . mysqli_error());
+    $dbc = mysqli_connect("localhost", "root", "", "projekt") or die('Error connecting to MySQL server' . mysqli_error());
     mysqli_set_charset($dbc, "utf8");
 ?>
